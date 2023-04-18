0acba9f60600821ba26fea88c783347cf7e20c82
LeeChungWoo <ios.lllccww@gmail.com>
2023-04-18 22:13:53 +0900

    Add: 구글맵 일단 띄워보기!

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,11 +9,22 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  // latiude - 위도
+  // logitude - 경도
+  static final LatLng companyLatLng = LatLng(
+    37.5233273,
+    126.921252,
+  );
+  static final CameraPosition initialPosition = CameraPosition(
+    target: companyLatLng,
+    zoom: 15,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
+      body: GoogleMap(
+        initialCameraPosition: initialPosition,
       ),
     );
   }
