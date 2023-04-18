95940a681f31ba5280a5863a111f5cfd473e8989
LeeChungWoo <ios.lllccww@gmail.com>
2023-04-18 23:25:03 +0900

    Add: 권한체크 함수 추가

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -33,6 +34,31 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
       ),
     );
+  }
+
+  Future<String> checkPermission() async {
+    final isLocationEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if (!isLocationEnabled) {
+      return '위치 서비스를 활성화 해주세요.';
+    }
+
+    LocationPermission checkPermission = await Geolocator.checkPermission();
+
+    if(checkPermission == LocationPermission.denied){
+      checkPermission = await Geolocator.requestPermission();
+
+      if(checkPermission == LocationPermission.denied){
+        return '위치 권한을 허가해주세요.';
+      }
+    }
+
+    if(checkPermission == LocationPermission.deniedForever){
+      return '앱의 위치 권한을 세팅에서 허가해주세요.';
+    }
+
+    return '위치 권한이 허가되었습니다.';
+
   }
 
   AppBar renderAppbar() {
